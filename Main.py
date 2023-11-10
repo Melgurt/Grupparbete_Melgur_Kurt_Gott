fa407c4807b3c352a53f68392e661b12e923df99
Melgurt <melker.vallen@gmail.com>
2023-11-10 09:16:47 +0100

    Main med förklaring

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Det här är huvudstrukturen vi håller oss till i programmet 
+# Men vi kommer såklart behöver hitta åp lite annat ibland    
+
+    # Print the main menu
+
+    # Print the first part of the story
+
+    # Grant the player with a choice 
+
+    # Create an if, elif and else statement
+
+    # Print more story
+
+    # Grant Choice 
+
+    # Create an if, elif and else statement
