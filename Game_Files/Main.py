c617ee8ac9baacfc0c035226a0bccf4bee7a7ce4
Melgurt <melker.vallen@gmail.com>
2023-11-21 14:16:17 +0100

    Melker Programmering 21/11

--- Game_Files/Main.py
+++ Game_Files/Main.py
@@ -6,11 +6,27 @@
 
     # Print the first part of the story
 
+print("story blalal")
+
     # Grant the player with a choice 
+
+print("You are granted with a choice where you can choose two paths. Please enter right or left")
+first_choice = input(">")
 
     # Create an if, elif and else statement
 
+if(first_choice == "right"):
+    print("You chose right")
+
+elif (first_choice == "left"):
+    print("You chose left")
+
+else:
+    print("Please enter right or left")
+
     # Print more story
+
+print("More story bblblbl")
 
     # Grant Choice 
 
